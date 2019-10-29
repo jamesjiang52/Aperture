5af03bb8aafcba0f0307ec25283875ed75981251
James Jiang <jamesjiang52@gmail.com>
2019-10-29 19:50:16 -0400

    Add mouse config and misc changes

--- game/output_controller.py
+++ game/output_controller.py
@@ -1,17 +1,19 @@
 import numpy as np
 import mss
+import time
 
 sct = mss.mss()
 
 
 def get_screenshot():
     # img has height equal to screen height and width equal to screen width
-    # (currently 640x360)
+    # (currently 640x480)
     # each element of img is an array of BGRA values, which we convert to RGB
     img = np.array(sct.grab(sct.monitors[0]), dtype=np.uint8)
     return np.flip(img[:, :, :3], 2)
 
 
 if __name__ == "__main__":
+    time.sleep(5)
     img = get_screenshot()
     print("Game resolution is {}x{}".format(len(img[0]), len(img)))
