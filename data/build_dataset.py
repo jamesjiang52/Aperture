227b8d09b6a2553fd978dedd7e7646b7d0ab19af
James Jiang <jamesj.jiang@mail.utoronto.ca>
2019-10-31 23:11:07 -0400

    Resize images, add trained model files

--- data/build_dataset.py
+++ data/build_dataset.py
@@ -1,8 +1,18 @@
+import os
+from PIL import Image
 from tflearn.data_utils import build_hdf5_image_dataset
 
+image_shape = (32, 32)
 dataset_file = "classes.txt"
-image_shape = (480, 480)
 output_path = "dataset.h5"
+
+for file_name in os.listdir("."):
+    if os.path.isdir(file_name):
+        for image_name in os.listdir(file_name):
+            image_path = "{}/{}".format(file_name, image_name)
+            image = Image.open(image_path)
+            image = image.resize(image_shape)
+            image.save(image_path)
 
 build_hdf5_image_dataset(
     dataset_file,
--- data/classifier.py
+++ data/classifier.py
@@ -5,9 +5,8 @@ from tflearn.layers import core, conv, estimator
 from tflearn.models import dnn
 
 
-h5f = h5py.File("dataset.h5", "r")
-X = h5f["X"]
-Y = h5f["Y"]
+dataset_filepath = "dataset.h5"
+model_filepath = "classifier.tfl"
 
 data_preprocessing = tflearn.data_preprocessing.DataPreprocessing()
 data_preprocessing.add_featurewise_stdnorm()
@@ -18,37 +17,44 @@ data_augmentation.add_random_flip_leftright()
 data_augmentation.add_random_flip_updown()
 data_augmentation.add_random_rotation(max_angle=180)
 
-# shape is 480x480x3
-network = core.input_data(shape=(None, 480, 480, 3), data_preprocessing=data_preprocessing, data_augmentation=data_augmentation)
-network = conv.conv_2d(network, nb_filter=6, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
-network = conv.conv_2d(network, nb_filter=6, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
-network = conv.max_pool_2d(network, kernel_size=4, strides=4, padding="same")
-
-# shape is 120x120x6
-network = conv.conv_2d(network, nb_filter=12, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
-network = conv.conv_2d(network, nb_filter=12, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
+network = core.input_data(shape=(None, 32, 32, 3), data_preprocessing=data_preprocessing, data_augmentation=data_augmentation)
+network = conv.conv_2d(network, nb_filter=32, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
+network = conv.conv_2d(network, nb_filter=32, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
 network = conv.max_pool_2d(network, kernel_size=2, strides=2, padding="same")
 
-# shape is 60x60x12
-network = conv.conv_2d(network, nb_filter=24, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
-network = conv.conv_2d(network, nb_filter=24, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
+network = conv.conv_2d(network, nb_filter=64, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
+network = conv.conv_2d(network, nb_filter=64, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
 network = conv.max_pool_2d(network, kernel_size=2, strides=2, padding="same")
 
-# shape is 30x30x24
-network = conv.conv_2d(network, nb_filter=48, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
-network = conv.conv_2d(network, nb_filter=48, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
+network = conv.conv_2d(network, nb_filter=128, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
+network = conv.conv_2d(network, nb_filter=128, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
 network = conv.max_pool_2d(network, kernel_size=2, strides=2, padding="same")
 
-# shape is 15x15x48
-network = conv.conv_2d(network, nb_filter=96, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
-network = conv.conv_2d(network, nb_filter=96, filter_size=5, strides=1, padding="same", activation="relu", regularizer="L2")
-network = conv.max_pool_2d(network, kernel_size=3, strides=3, padding="same")
+network = conv.conv_2d(network, nb_filter=256, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
+network = conv.conv_2d(network, nb_filter=256, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
+network = conv.max_pool_2d(network, kernel_size=2, strides=2, padding="same")
 
-# shape is 5x5x96
-network = core.fully_connected(network, n_units=256, activation="relu", regularizer="L2")
-network = core.fully_connected(network, n_units=256, activation="relu", regularizer="L2")
+network = conv.conv_2d(network, nb_filter=512, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
+network = conv.conv_2d(network, nb_filter=512, filter_size=3, strides=1, padding="same", activation="relu", regularizer="L2")
+network = conv.max_pool_2d(network, kernel_size=2, strides=2, padding="same")
+
+network = core.fully_connected(network, n_units=1024, activation="relu", regularizer="L2")
+network = core.fully_connected(network, n_units=1024, activation="relu", regularizer="L2")
 network = core.fully_connected(network, n_units=6, activation="linear", regularizer="L2")
-network = estimator.regression(network, optimizer="adam", loss="softmax_categorical_crossentropy")
+network = estimator.regression(network, optimizer="adam", loss="softmax_categorical_crossentropy", learning_rate=0.001)
 
 model = dnn.DNN(network)
-model.fit(X, Y, validation_set=0.2, show_metric=True)
+
+
+def train_model():
+    h5f = h5py.File(dataset_filepath, "r")
+    X = h5f["X"]
+    Y = h5f["Y"]
+
+    model.fit(X, Y, n_epoch=200, validation_set=0.2, show_metric=True)
+    model.save(model_filepath)
+    
+    
+def predict_label(X):
+    model.load(model_filepath)
+    model.predict_label(X)
